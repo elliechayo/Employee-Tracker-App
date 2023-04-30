bd12c95ad6518ea5d20dcd915a539d1d37ad71e3
Elinor Chayo <elliechayo@gmail.com>
2023-04-30 13:57:41 -0400

    created the fetch calls within the index.js to return all departments, employees and roles data

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
+// fetch and return all departments from database
+const getDepartments = function () {
+     connection
+       .promise()
+       .query("SELECT * FROM `department`")
+       .then(([rows, fields]) => {
+         console.table(rows);
+         main();
+       })
+       .catch(console.log);
+   };
 
+   // fetch and return all employees from database
+const getEmployees = function () {
+     connection
+       .promise()
+       .query(
+         "SELECT employee.id, first_name, last_name, role_id, manager_id, role.title, role.salary, role.department_id FROM `employee` INNER JOIN `role` ON (employee.role_id=role.id)"
+       )
+       .then(([rows, fields]) => {
+         console.table(rows);
+         main();
+       })
+       .catch(console.log);
+   };
+
+
+   // fetch and return all roles from database
+const getRoles = function () {
+     connection
+       .promise()
+       .query(
+         "SELECT role.id, title, salary, department_id, department.name FROM `role` INNER JOIN department ON (role.department_id = department.id) ORDER BY role.id ASC"
+       )
+       .then(([rows, fields]) => {
+         console.table(rows);
+         main();
+       })
+       .catch(console.log);
+   };
+
+   // fetch and return all roles from database
+const getRoles = function () {
+     connection
+       .promise()
+       .query(
+         "SELECT role.id, title, salary, department_id, department.name FROM `role` INNER JOIN department ON (role.department_id = department.id) ORDER BY role.id ASC"
+       )
+       .then(([rows, fields]) => {
+         console.table(rows);
+         main();
+       })
+       .catch(console.log);
+   };
+
+   
